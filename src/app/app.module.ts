6110cb8ec7c1ec20408d764164b43e9e31b5f709
NJOKU <henrietha.njoku@clearchannelint.com>
2022-02-11 23:42:29 +0000

    recipe-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
